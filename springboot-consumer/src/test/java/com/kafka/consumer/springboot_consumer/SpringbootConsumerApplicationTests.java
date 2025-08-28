212fda57d8e93c7a7d4fa9b38d72f952a0f60375
iscipar <iscipar@hotmail.com>
2025-08-28 16:25:54 +0000

    Proyecto inicial creado con Spring Initializr

---
+++ springboot-consumer/src/test/java/com/kafka/consumer/springboot_consumer/SpringbootConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.consumer.springboot_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ springboot-producer/src/main/java/com/kafka/producer/springboot_producer/SpringbootProducerApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.producer.springboot_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProducerApplication.class, args);
+	}
+
+}
